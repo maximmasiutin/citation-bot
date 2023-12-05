d88c8ac0a6d92236a72ad16657c43628aed38b15
Maxim Masiutin <maxim@masiutin.com>
2023-12-05 17:33:06 +0200

    body save

--- expandFns.php
+++ expandFns.php
@@ -1227,7 +1227,7 @@ function edit_a_list_of_pages(array $pages_in_category, WikipediaBot $api, strin
         if (file_put_contents($filename, $body)===$bodylen)
         {
           report_phase("Saved to file " . echoable($filename));
-        }
+        } else
         {
           report_warning("Save to file failed.");
         }
